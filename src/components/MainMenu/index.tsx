798621b409b8ec877392fb80891d476ed492e91b
Nikolay <nick.zhigalin@gmail.com>
2019-05-10 19:01:18 +0300

    Компонент основного меню + интерфейсы

---
+++ src/components/MainMenu/index.tsx
@@ -0,0 +1,28 @@
+import React, { Component, ReactNode } from 'react';
+import { WithStyles } from '@material-ui/core';
+import { styles } from './styles';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+interface Props extends WithStyles<typeof styles> {
+}
+
+interface State {
+}
+
+class MainMenu extends Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+		this.state = {}
+	}
+
+	public render(): ReactNode {
+		const { classes } = this.props;
+		return (
+			<div>
+				Привет
+			</div>
+		);
+	}
+}
+
+export default withStyles(styles)(MainMenu);
---
+++ src/lib/types.ts
@@ -0,0 +1,24 @@
+export interface Employee {
+	id?: number;
+	first_name: string;
+	last_name: string;
+	middle_name: string | null;
+	phone: string | null;
+	age: number;
+	date_of_birth: Date;
+	registration_date: Date;
+	organization: number | null;
+}
+
+export interface Organization {
+	id?: number;
+	full_name: string;
+	short_name: string;
+	registration_date: Date;
+	inn: string;
+	kpp: string;
+	ogrn: string;
+	okved_code: string;
+	okved_name: string;
+	employees: Employee[];
+}
