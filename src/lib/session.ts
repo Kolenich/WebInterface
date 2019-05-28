3fa937a53eb011366e81de8cc344b0d3933d41d8
Nikolay <nick.zhigalin@gmail.com>
2019-05-28 23:44:38 +0300

    Правка конфигов для деплоя

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -3,11 +3,18 @@ import { OutgoingHttpHeaders } from 'http';
 
 const xsrfCookieName: string = 'csrftoken';
 const xsrfHeaderName: string = 'X-CSRFToken';
-const baseURL: string = 'http://localhost:8000/';
+
+let baseURL: string = 'http://localhost:8000/';
+if (process.env.NODE_ENV === 'production') {
+  baseURL = 'http://localhost/server';
+}
+
 const timeout: number = 100000;
+
 const headers: OutgoingHttpHeaders = {
   'Content-Type': 'application/json',
 };
+
 const sessionConfig: AxiosRequestConfig = {
   baseURL,
   xsrfCookieName,
