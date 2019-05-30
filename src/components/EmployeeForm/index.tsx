e6c6735374e38a6c3c7a69ea0ad7bda4dc8a4bca
Nikolay <nick.zhigalin@gmail.com>
2019-05-30 21:46:37 +0300

    Хрен пойми что

--- src/components/EmployeeForm/index.tsx
+++ src/components/EmployeeForm/index.tsx
@@ -64,10 +64,10 @@ class EditEmployee extends PureComponent<Props, State> {
     const { id } = this.props;
     if (id !== -1) {
       api.getContent<Employee>(`employees/${id}`)
-        .then(((response: AxiosResponse<Employee>) => {
-          this.setState({ ...response.data });
-        }))
-        .catch();
+          .then(((response: AxiosResponse<Employee>) => {
+            this.setState({ ...response.data });
+          }))
+          .catch();
     }
   }
 
@@ -76,10 +76,10 @@ class EditEmployee extends PureComponent<Props, State> {
     if (prevProps.id !== id) {
       if (id !== -1) {
         api.getContent<Employee>(`employees/${id}`)
-          .then(((response: AxiosResponse<Employee>) => {
-            this.setState({ ...response.data });
-          }))
-          .catch();
+            .then(((response: AxiosResponse<Employee>) => {
+              this.setState({ ...response.data });
+            }))
+            .catch();
       } else {
         this.setState({
           first_name: '',
@@ -105,8 +105,8 @@ class EditEmployee extends PureComponent<Props, State> {
     const { classes } = this.props;
     const employee = { ...this.state };
     const value: string = employee[props.fieldName] !== null && employee[props.fieldName] ?
-      String(employee[props.fieldName]) :
-      '';
+        String(employee[props.fieldName]) :
+        '';
     let valid: boolean = true;
     let helperText: string = '';
     if (props.validationType && value !== '') {
@@ -116,22 +116,22 @@ class EditEmployee extends PureComponent<Props, State> {
       if (!valid) helperText = validationMessages[props.validationType];
     }
     return (
-      <Grid item xs={props.xs}>
-        <TextField
-          label={employeeLabel[props.fieldName]}
-          placeholder={employeeLabel[props.fieldName]}
-          className={classes.textField}
-          name={String(props.fieldName)}
-          error={!valid}
-          helperText={helperText}
-          fullWidth
-          margin="normal"
-          variant="outlined"
-          onChange={this.handleAttributeChange(props.fieldName)}
-          value={value}
-          required={props.required}
-        />
-      </Grid>
+        <Grid item xs={props.xs}>
+          <TextField
+              label={employeeLabel[props.fieldName]}
+              placeholder={employeeLabel[props.fieldName]}
+              className={classes.textField}
+              name={String(props.fieldName)}
+              error={!valid}
+              helperText={helperText}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              onChange={this.handleAttributeChange(props.fieldName)}
+              value={value}
+              required={props.required}
+          />
+        </Grid>
     );
   }
 
@@ -139,27 +139,27 @@ class EditEmployee extends PureComponent<Props, State> {
     const { classes } = this.props;
     const employee = { ...this.state };
     const value: string = employee[props.fieldName] !== null && employee[props.fieldName] ?
-      String(employee[props.fieldName]) :
-      String(new Date());
+        String(employee[props.fieldName]) :
+        String(new Date());
     return (
-      <Grid item xs={props.xs}>
-        <MuiPickersUtilsProvider utils={DateFnsUtils} locale={ruLocale}>
-          <InlineDatePicker
-            className={classes.datePicker}
-            margin="normal"
-            variant="outlined"
-            label={employeeLabel[props.fieldName]}
-            value={value}
-            onChange={this.handleDateChange(props.fieldName)}
-            format="dd.MM.yyyy"
-            keyboard
-            disableFuture
-            invalidDateMessage="Дата должна быть в формате ДД.ММ.ГГГГ"
-            minDateMessage="Дата не должна быть меньше 01.01.1900"
-            maxDateMessage="Дата не должна превышать сегодняшнее число"
-          />
-        </MuiPickersUtilsProvider>
-      </Grid>
+        <Grid item xs={props.xs}>
+          <MuiPickersUtilsProvider utils={DateFnsUtils} locale={ruLocale}>
+            <InlineDatePicker
+                className={classes.datePicker}
+                margin="normal"
+                variant="outlined"
+                label={employeeLabel[props.fieldName]}
+                value={value}
+                onChange={this.handleDateChange(props.fieldName)}
+                format="dd.MM.yyyy"
+                keyboard
+                disableFuture
+                invalidDateMessage="Дата должна быть в формате ДД.ММ.ГГГГ"
+                minDateMessage="Дата не должна быть меньше 01.01.1900"
+                maxDateMessage="Дата не должна превышать сегодняшнее число"
+            />
+          </MuiPickersUtilsProvider>
+        </Grid>
     );
   }
 
@@ -167,21 +167,21 @@ class EditEmployee extends PureComponent<Props, State> {
     const { classes } = this.props;
     const employee = { ...this.state };
     const value: string = employee[props.fieldName] !== null && employee[props.fieldName] ?
-      String(employee[props.fieldName]) :
-      '';
+        String(employee[props.fieldName]) :
+        '';
     return (
-      <Grid item xs={props.xs}>
-        <FormControl variant="outlined" className={classes.formControl} required={props.required}>
-          <InputLabel htmlFor="sex">Пол</InputLabel>
-          <Select
-            value={value}
-            onChange={this.handleSelectChange}
-            input={<OutlinedInput labelWidth={30} id="sex"/>}>
-            <MenuItem value="male">Мужской</MenuItem>
-            <MenuItem value="female">Женский</MenuItem>
-          </Select>
-        </FormControl>
-      </Grid>
+        <Grid item xs={props.xs}>
+          <FormControl variant="outlined" className={classes.formControl} required={props.required}>
+            <InputLabel htmlFor="sex">Пол</InputLabel>
+            <Select
+                value={value}
+                onChange={this.handleSelectChange}
+                input={<OutlinedInput labelWidth={30} id="sex"/>}>
+              <MenuItem value="male">Мужской</MenuItem>
+              <MenuItem value="female">Женский</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
     );
   }
 
@@ -189,46 +189,46 @@ class EditEmployee extends PureComponent<Props, State> {
     const { classes } = this.props;
     const { first_name, last_name, email, sex } = this.state;
     const disabled: boolean =
-      validationMethods.cyrillic(first_name) &&
-      validationMethods.cyrillic(last_name) &&
-      validationMethods.email(email) &&
-      sex !== '';
+        validationMethods.cyrillic(first_name) &&
+        validationMethods.cyrillic(last_name) &&
+        validationMethods.email(email) &&
+        sex !== '';
     return (
-      <Button
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        disabled={!disabled}
-        {...buttonProps}
-      >
-        {buttonProps.text}
-        {buttonProps.icon === 'save' &&
-        <Save className={classes.rightIcon}/>}
-        {buttonProps.icon === 'add' &&
-        <Add className={classes.rightIcon}/>}
-        {buttonProps.icon === 'confirm' &&
-        <Done className={classes.rightIcon}/>}
-        {buttonProps.icon === 'update' &&
-        <Update className={classes.rightIcon}/>}
-      </Button>
+        <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            disabled={!disabled}
+            {...buttonProps}
+        >
+          {buttonProps.text}
+          {buttonProps.icon === 'save' &&
+			    <Save className={classes.rightIcon}/>}
+          {buttonProps.icon === 'add' &&
+			    <Add className={classes.rightIcon}/>}
+          {buttonProps.icon === 'confirm' &&
+			    <Done className={classes.rightIcon}/>}
+          {buttonProps.icon === 'update' &&
+			    <Update className={classes.rightIcon}/>}
+        </Button>
     );
   }
 
   SecondaryButton = (buttonProps: CustomButtonProps): ReactElement<ReactNode> => {
     const { classes } = this.props;
     return (
-      <Button
-        variant="contained"
-        color="secondary"
-        className={classes.button}
-        {...buttonProps}
-      >
-        {buttonProps.text}
-        {buttonProps.icon === 'delete' &&
-        <Delete className={classes.rightIcon}/>}
-        {buttonProps.icon === 'cancel' &&
-        <Cancel className={classes.rightIcon}/>}
-      </Button>
+        <Button
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+            {...buttonProps}
+        >
+          {buttonProps.text}
+          {buttonProps.icon === 'delete' &&
+			    <Delete className={classes.rightIcon}/>}
+          {buttonProps.icon === 'cancel' &&
+			    <Cancel className={classes.rightIcon}/>}
+        </Button>
     );
   }
 
@@ -236,30 +236,30 @@ class EditEmployee extends PureComponent<Props, State> {
     const { successWindow, errorWindow, statusMessage } = this.state;
     const { classes } = this.props;
     return (
-      <Dialog open={successWindow || errorWindow} scroll="paper">
-        <DialogTitle disableTypography>
-          {successWindow &&
-          <Typography variant="h5" className={classes.message}>
-	          <CheckCircle className={classNames(classes.statusIcon, classes.successIcon)}/>
-	          Успешно
-          </Typography>}
-          {errorWindow &&
-          <Typography variant="h5" className={classes.message}>
-	          <Error className={classNames(classes.statusIcon, classes.errorIcon)}/>
-	          Ошибка
-          </Typography>}
-        </DialogTitle>
-        <DialogContent>
-          {statusMessage}
-        </DialogContent>
-        <DialogActions>
-          <this.PrimaryButton
-            text="Ок"
-            onClick={this.closeStatusModal}
-            icon="confirm"
-          />
-        </DialogActions>
-      </Dialog>
+        <Dialog open={successWindow || errorWindow} scroll="paper">
+          <DialogTitle disableTypography>
+            {successWindow &&
+				    <Typography variant="h5" className={classes.message}>
+					    <CheckCircle className={classNames(classes.statusIcon, classes.successIcon)}/>
+					    Успешно
+				    </Typography>}
+            {errorWindow &&
+				    <Typography variant="h5" className={classes.message}>
+					    <Error className={classNames(classes.statusIcon, classes.errorIcon)}/>
+					    Ошибка
+				    </Typography>}
+          </DialogTitle>
+          <DialogContent>
+            {statusMessage}
+          </DialogContent>
+          <DialogActions>
+            <this.PrimaryButton
+                text="Ок"
+                onClick={this.closeStatusModal}
+                icon="confirm"
+            />
+          </DialogActions>
+        </Dialog>
     );
   }
 
@@ -280,23 +280,23 @@ class EditEmployee extends PureComponent<Props, State> {
   }
 
   private handleAttributeChange =
-    (name: keyof State) => (event: ChangeEvent<HTMLInputElement>): ComponentState => {
-      this.setState({ [name]: event.target.value });
-    }
+      (name: keyof State) => (event: ChangeEvent<HTMLInputElement>): ComponentState => {
+        this.setState({ [name]: event.target.value });
+      }
 
   private deleteForm = (): ComponentState => {
     const employee: Employee = { ...this.state };
     const { deleteRecord } = this.props;
     api.sendContent<Employee>('employees', employee, employee.id, 'delete')
-      .then(() => {
-        if (employee.id) {
-          deleteRecord(employee.id);
-          this.setState({ successWindow: true, statusMessage: DELETE_SUCCESS });
-        }
-      })
-      .catch((error: AxiosError) => {
-        if (error.response) console.log(error.response.data);
-      });
+        .then(() => {
+          if (employee.id) {
+            deleteRecord(employee.id);
+            this.setState({ successWindow: true, statusMessage: DELETE_SUCCESS });
+          }
+        })
+        .catch((error: AxiosError) => {
+          if (error.response) console.log(error.response.data);
+        });
   }
 
   private submitForm = (): ComponentState => {
@@ -309,71 +309,71 @@ class EditEmployee extends PureComponent<Props, State> {
     employee.date_of_birth = moment(employee.date_of_birth).format('YYYY-MM-DD');
     if (employee.id) {
       api.sendContent<Employee>('employees', employee, employee.id, 'patch')
-        .then((response: AxiosResponse<Employee>) => {
-          const newEmployee: Employee = response.data;
-          updateTable(newEmployee);
-          this.setState({ successWindow: true, statusMessage: UPDATE_SUCCESS });
-        })
-        .catch((error: AxiosError) => {
-          if (error.response) {
-            this.setState({ errorWindow: true, statusMessage: error.response.data });
-          }
-        });
+          .then((response: AxiosResponse<Employee>) => {
+            const newEmployee: Employee = response.data;
+            updateTable(newEmployee);
+            this.setState({ successWindow: true, statusMessage: UPDATE_SUCCESS });
+          })
+          .catch((error: AxiosError) => {
+            if (error.response) {
+              this.setState({ errorWindow: true, statusMessage: error.response.data });
+            }
+          });
     } else {
       api.sendContent<Employee>('employees', employee)
-        .then((response: AxiosResponse<Employee>) => {
-          const newEmployee: Employee = response.data;
-          updateTable(newEmployee);
-          this.setState({ successWindow: true, statusMessage: SAVE_SUCCESS });
-        })
-        .catch((error: AxiosError) => {
-          if (error.response) {
-            console.log(error.response.data);
-            this.setState({ errorWindow: true, statusMessage: SERVER_ERROR });
-          }
-        });
+          .then((response: AxiosResponse<Employee>) => {
+            const newEmployee: Employee = response.data;
+            updateTable(newEmployee);
+            this.setState({ successWindow: true, statusMessage: SAVE_SUCCESS });
+          })
+          .catch((error: AxiosError) => {
+            if (error.response) {
+              console.log(error.response.data);
+              this.setState({ errorWindow: true, statusMessage: SERVER_ERROR });
+            }
+          });
     }
   }
 
   public render(): ReactNode {
     const { id, onCLose, open } = this.props;
     const title: string = id !== -1 ?
-      'Редактировать сотрудника' :
-      'Зарегистрировать сотрудника';
+        'Редактировать сотрудника' :
+        'Зарегистрировать сотрудника';
     return (
-      <Dialog open={open} onClose={onCLose}>
-        <DialogTitle>{title}</DialogTitle>
-        <DialogContent>
-          <this.StatusModal/>
-          <Grid container spacing={spacing}>
-            <this.InputField xs={4} fieldName="last_name" required validationType="cyrillic"/>
-            <this.InputField xs={4} fieldName="first_name" required validationType="cyrillic"/>
-            <this.InputField xs={4} fieldName="middle_name" validationType="cyrillic"/>
-          </Grid>
-          <Grid container spacing={spacing}>
-            <this.InputField xs={5} fieldName="email" required validationType="email"/>
-            <this.InputField xs={4} fieldName="phone" validationType="phone"/>
-            <this.SelectField xs={3} fieldName="sex" required/>
-          </Grid>
-          <Grid container spacing={spacing}>
-            <this.DateField xs={4} fieldName="date_of_birth"/>
-          </Grid>
-        </DialogContent>
-        <DialogActions>
-          <this.PrimaryButton
-            text={id !== -1 ?
-              'Сохранить' :
-              'Создать'}
-            icon={id !== -1 ?
-              'save' :
-              'add'}
-            onClick={this.submitForm}/>
-          {id !== -1 &&
-          <this.SecondaryButton text="Удалить" icon="delete" onClick={this.deleteForm}/>}
-          <this.SecondaryButton text="Отмена" icon="cancel"
-                                onClick={onCLose}/>
-        </DialogActions>
-      </Dialog>
+        <Dialog open={open} onClose={onCLose}>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogContent>
+            <this.StatusModal/>
+            <Grid container spacing={spacing}>
+              <this.InputField xs={4} fieldName="last_name" required validationType="cyrillic"/>
+              <this.InputField xs={4} fieldName="first_name" required validationType="cyrillic"/>
+              <this.InputField xs={4} fieldName="middle_name" validationType="cyrillic"/>
+            </Grid>
+            <Grid container spacing={spacing}>
+              <this.InputField xs={5} fieldName="email" required validationType="email"/>
+              <this.InputField xs={4} fieldName="phone" validationType="phone"/>
+              <this.SelectField xs={3} fieldName="sex" required/>
+            </Grid>
+            <Grid container spacing={spacing}>
+              <this.DateField xs={4} fieldName="date_of_birth"/>
+            </Grid>
+          </DialogContent>
+          <DialogActions>
+            <this.PrimaryButton
+                text={id !== -1 ?
+                    'Сохранить' :
+                    'Создать'}
+                icon={id !== -1 ?
+                    'save' :
+                    'add'}
+                onClick={this.submitForm}/>
+            {id !== -1 &&
+				    <this.SecondaryButton text="Удалить" icon="delete" onClick={this.deleteForm}/>}
+            <this.SecondaryButton text="Отмена" icon="cancel"
+                                  onClick={onCLose}/>
+          </DialogActions>
+        </Dialog>
     );
   }
 }
