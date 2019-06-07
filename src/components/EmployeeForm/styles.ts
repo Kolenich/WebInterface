e541d6126a510c4efaf8652d540cf2e21bdaac92
Nikolay <nick.zhigalin@gmail.com>
2019-06-07 23:10:21 +0300

    Удадление лишних классов стилей + реформат

--- src/components/EmployeeForm/styles.ts
+++ src/components/EmployeeForm/styles.ts
@@ -1,6 +1,5 @@
 import { Theme } from '@material-ui/core';
 import { createStyles } from '@material-ui/core/styles';
-import { amber, green } from '@material-ui/core/colors';
 
 export const styles = (theme: Theme) => createStyles({
   textField: {
@@ -20,23 +19,5 @@ export const styles = (theme: Theme) => createStyles({
   },
   rightIcon: {
     marginLeft: theme.spacing.unit,
-  },
-  statusIcon: {
-    fontSize: 35,
-    opacity: 0.9,
-    marginRight: theme.spacing.unit,
-  },
-  errorIcon: {
-    color: theme.palette.error.dark,
-  },
-  warningIcon: {
-    color: amber[600],
-  },
-  successIcon: {
-    color: green[600],
-  },
-  message: {
-    display: 'flex',
-    alignItems: 'center',
   },
 });
