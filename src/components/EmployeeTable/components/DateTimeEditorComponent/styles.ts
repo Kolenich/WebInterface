60e53e11012b156b90c2a1fd65175e61183af36c
Nikolay <nick.zhigalin@gmail.com>
2019-08-03 17:57:00 +0300

    Объекты стилей создаются через функцию createStyles

--- src/components/EmployeeTable/components/DateTimeEditorComponent/styles.ts
+++ src/components/EmployeeTable/components/DateTimeEditorComponent/styles.ts
@@ -1,6 +1,6 @@
-import { Theme } from '@material-ui/core';
+import { createStyles, Theme } from '@material-ui/core';
 
-export const styles = (theme: Theme) => ({
+export const styles = (theme: Theme) => createStyles({
   datePicker: {
     marginBottom: theme.spacing(2),
     width: '100%',
--- src/components/EmployeeTable/components/TextEditorComponent/styles.ts
+++ src/components/EmployeeTable/components/TextEditorComponent/styles.ts
@@ -1,6 +1,6 @@
-import { Theme } from '@material-ui/core';
+import { createStyles, Theme } from '@material-ui/core';
 
-export const styles = (theme: Theme) => ({
+export const styles = (theme: Theme) => createStyles({
   textField: {
     marginBottom: theme.spacing(2),
   },
