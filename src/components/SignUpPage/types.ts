a4b31c96709c5d55351eae2cdf87a60bb8c43074
Nikolay <nick.zhigalin@gmail.com>
2019-08-23 14:44:01 +0300

    Компонент страницы регистрации переписан как классовый

--- src/components/SignUpPage/types.ts
+++ src/components/SignUpPage/types.ts
@@ -1,7 +1,26 @@
+import { WithStyles } from '@material-ui/styles';
 import { IVariantIcons } from 'lib/generic/Snackbar/types';
 import { RouteComponentProps } from 'react-router-dom';
+import { styles } from './styles';
 
-export interface IProps extends RouteComponentProps {
+export interface IProps extends WithStyles<typeof styles>, RouteComponentProps {
 }
 
-export type ISnackbarVariant = keyof IVariantIcons;
+export interface IState {
+  /** Имя */
+  first_name: string;
+  /** Фамилия */
+  last_name: string;
+  /** Электронная почта */
+  email: string;
+  /** Пароль */
+  password: string;
+  /** Флаг загрузки */
+  loading: boolean;
+  /** Флаг открытия снэкбара */
+  snackbarOpen: boolean;
+  /** Сообщение снэкбара */
+  snackbarMessage: string;
+  /** Статус снэкбара */
+  snackbarVariant: keyof IVariantIcons;
+}
