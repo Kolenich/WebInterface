a3b6e57182b4e24065720b1e958f528a62d3192a
Nikolay <nick.zhigalin@gmail.com>
2019-10-08 14:15:00 +0300

    Типизация декоратора

--- src/decorators/notification/index.tsx
+++ src/decorators/notification/index.tsx
@@ -2,64 +2,65 @@ import Dialog from 'lib/generic/Dialog';
 import Snackbar from 'lib/generic/Snackbar';
 import { IVariantIcons } from 'lib/generic/Snackbar/types';
 import { IDialogProps, IDialogStatus, ISnackbarProps } from 'lib/types';
-import React, { ComponentType, useState } from 'react';
+import React, { ComponentType, FC, useState } from 'react';
 import { INotifications } from './types';
 
 /**
  * Декоратор, подмешивающий компоненту диалоговое окно и снэкбар с функциями для их вызова
  * @param Component оборачиваемый компонент
  */
-const withNotification = <T extends INotifications>(Component: ComponentType<T>) => (props: T) => {
-  // Переменные состояния для снэкбара
-  const [snackbar, setSnackbar] = useState<ISnackbarProps>({
-    open: false,
-    variant: 'info',
-    message: '',
-  });
+const withNotification =
+  <T extends INotifications>(Component: ComponentType<T>): FC<T> => (props: T): JSX.Element => {
+    // Переменные состояния для снэкбара
+    const [snackbar, setSnackbar] = useState<ISnackbarProps>({
+      open: false,
+      variant: 'info',
+      message: '',
+    });
 
-  // Переменные состояния для снэкбара
-  const [dialog, setDialog] = useState<IDialogProps>({
-    open: false,
-    status: 'loading',
-    message: '',
-  });
+    // Переменные состояния для снэкбара
+    const [dialog, setDialog] = useState<IDialogProps>({
+      open: false,
+      status: 'loading',
+      message: '',
+    });
 
-  /**
-   * Функция, закрывающая диалог
-   */
-  const closeDialog = (): void => setDialog({ ...dialog, open: false });
+    /**
+     * Функция, закрывающая диалог
+     */
+    const closeDialog = (): void => setDialog({ ...dialog, open: false });
 
-  /**
-   * Функция, закрывающая снэкбар
-   */
-  const closeSnackbar = (): void => setSnackbar({ ...snackbar, open: false });
+    /**
+     * Функция, закрывающая снэкбар
+     */
+    const closeSnackbar = (): void => setSnackbar({ ...snackbar, open: false });
 
-  /**
-   * Функция вызова диалогового окна
-   * @param status статус вызываемого окна
-   * @param message сообщение
-   */
-  const openDialog = (status: IDialogStatus, message: string = ''): void => (
-    setDialog({ status, message, open: true })
-  );
+    /**
+     * Функция вызова диалогового окна
+     * @param status статус вызываемого окна
+     * @param message сообщение
+     */
+    const openDialog = (status: IDialogStatus, message: string = ''): void => (
+      setDialog({ status, message, open: true })
+    );
 
-  /**
-   * Функция вызова снэкбара
-   * @param variant тип вызываемого снэкбара
-   * @param message сообщение
-   */
-  const openSnackbar = (variant: keyof IVariantIcons, message: string = ''): void => (
-    setSnackbar({ variant, message, open: true })
-  );
+    /**
+     * Функция вызова снэкбара
+     * @param variant тип вызываемого снэкбара
+     * @param message сообщение
+     */
+    const openSnackbar = (variant: keyof IVariantIcons, message: string = ''): void => (
+      setSnackbar({ variant, message, open: true })
+    );
 
-  return (
-    <>
-      <Dialog {...dialog} onClose={closeDialog} />
-      <Snackbar {...snackbar} onClose={closeSnackbar} />
-      <Component {...props} openDialog={openDialog} openSnackbar={openSnackbar} />
-    </>
-  );
+    return (
+      <>
+        <Dialog {...dialog} onClose={closeDialog} />
+        <Snackbar {...snackbar} onClose={closeSnackbar} />
+        <Component {...props} openDialog={openDialog} openSnackbar={openSnackbar} />
+      </>
+    );
 
-};
+  };
 
 export default withNotification;
