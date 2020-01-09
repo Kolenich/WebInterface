823e649516b660edf7d9ee411230ef5afca1d5b6
nikolay <zhigalin@fas.gov.ru>
2020-01-09 17:58:52 +0300

    Настройка роутера

--- src/router/DashBoardRouter/index.tsx
+++ src/router/DashBoardRouter/index.tsx
@@ -3,6 +3,7 @@ import TaskDetail from 'components/TaskDetail';
 import TasksTable from 'components/TasksTable';
 import React, { FC, memo } from 'react';
 import { Redirect, Switch } from 'react-router';
+import { HashRouter } from 'react-router-dom';
 import Route from 'router/Route';
 
 /**
@@ -12,10 +13,12 @@ import Route from 'router/Route';
  */
 const DashBoardRouter: FC = () => (
   <Switch>
-    <Redirect exact from="/" to="/my-tasks/in-process"/>
-    <Route inner path="/my-tasks/:filter(completed|in-process)" component={TasksTable}/>
-    <Route inner path="/assign" component={TaskAssignment}/>
-    <Route inner path="/my-tasks/:id" component={TaskDetail}/>
+    <HashRouter hashType="noslash">
+      <Redirect exact from="/" to="/my-tasks/in-process"/>
+      <Route inner path="/my-tasks/:filter(completed|in-process)" component={TasksTable}/>
+      <Route inner path="/assign" component={TaskAssignment}/>
+      <Route inner path="/my-tasks/:id" component={TaskDetail}/>
+    </HashRouter>
   </Switch>
 );
 
