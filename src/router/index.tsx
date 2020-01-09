867cec2421b9ef5bb10bcf4f10456797ee1c9154
Nikolay <nick.zhigalin@gmail.com>
2020-01-09 20:01:42 +0300

    Возвращен хэш роутер

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,6 +3,7 @@ import SignInPage from 'components/SignInPage';
 import SignUpPage from 'components/SignUpPage';
 import React, { FC, memo } from 'react';
 import { Switch } from 'react-router';
+import { HashRouter } from 'react-router-dom';
 import Route from './Route';
 
 /**
@@ -12,9 +13,11 @@ import Route from './Route';
  */
 const Router: FC = () => (
   <Switch>
-    <Route path="/sign-up" component={SignUpPage}/>
-    <Route path="/sign-in" component={SignInPage}/>
-    <Route inner path="/" component={DashBoard}/>
+    <HashRouter hashType="slash">
+      <Route path="/sign-up" component={SignUpPage}/>
+      <Route path="/sign-in" component={SignInPage}/>
+      <Route inner path="/" component={DashBoard}/>
+    </HashRouter>
   </Switch>
 );
 
